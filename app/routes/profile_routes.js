d34b4937e05a1fec24bb35af4f34e266847f84fe
devinrbopp <devinrbopp@gmail.com>
2021-12-11 15:21:49 -0700

    profiles add and index routes working

--- app/routes/profile_routes.js
+++ app/routes/profile_routes.js
@@ -3,34 +3,45 @@ const express = require('express')
 // Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
 
-// pull profiel database model
+// pull profile database model
 const Profile = require('../models/profile')
 
-// this is a collection of methods that help us detect situations when we need
-// to throw a custom error
+// customer error messages
 const customErrors = require('../../lib/custom_errors')
 
-// we'll use this function to send 404 when non-existant document is requested
 const handle404 = customErrors.handle404
-// we'll use this function to send 401 when a user tries to modify a resource
-// that's owned by someone else
+
+// sends 401 error
 const requireOwnership = customErrors.requireOwnership
 
-// this is middleware that will remove blank fields from `req.body`, e.g.
-// { example: { title: '', text: 'foo' } } -> { example: { text: 'foo' } }
 const removeBlanks = require('../../lib/remove_blank_fields')
+
 // passing this as a second argument to `router.<verb>` will make it
 // so that a token MUST be passed for that route to be available
 // it will also set `req.user`
 const requireToken = passport.authenticate('bearer', { session: false })
 
-// instantiate a router (mini app that only handles routes)
+// instantiate router
 const router = express.Router()
 
 
 // GET index route displays all profiles
-router.get('/profiles', (req, res) => {
-    res.json({message: 'Profiles Index'})
+router.get('/profiles', (req, res, next) => {
+    Profile.find()
+        .then((profiles) => {
+            res.status(200).json(profiles)
+        })
+        // .then((profiles) => res.status(200).json({ profiles: profiles }))
+        .catch(next)
+})
+
+// POST create a profile
+router.post('/profiles', (req, res, next) => {
+    Profile.create(req.body)
+        .then(createdProfile => {
+            res.status(201).json({ profile: createdProfile.toObject() })
+        })
+        .catch(next)
 })
 
 module.exports = router
